4b241c8cc70467044f165879911c37172809d839
Nelson Chinedu <egwuchinedu69@gmail.com>
2020-07-30 19:40:26 +0100

    add contact and event entities

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,6 @@
 import Account from './entity/Account';
 import Profile from './entity/Profile';
+import Event from './entity/Event';
+import Contact from './entity/Contact';
 
-export { Account, Profile };
+export { Account, Profile, Event, Contact };
