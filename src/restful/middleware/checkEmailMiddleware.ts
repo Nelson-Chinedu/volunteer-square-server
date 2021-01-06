0e64e300a90dcaebe4d6bf9e7c6c71a75e6b138b
Nelson Chinedu <egwuchinedu69@gmail.com>
2021-01-06 15:02:08 +0100

    check diposable email

--- src/restful/middleware/checkEmailMiddleware.ts
+++ src/restful/middleware/checkEmailMiddleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import winstonEnvLogger from 'winston-env-logger';
+import axios from 'axios';
 
 import { Account } from '../../db';
 
@@ -8,6 +9,13 @@ import { respondWithWarning } from '../../lib/httpResponse';
 export default async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
   try {
+    const {data}: any  = await axios.get(`https://block-temporary-email.com/check/email/${email}`);
+    if (data){
+      const {status, temporary} = await data;
+      if (status === 200 && temporary === true){
+        return respondWithWarning(res, 400, 'Please do not use disposable email address');
+      }
+    }
     const checkEmail = await Account.findOne({
       where: {
         email,
